b57f3b59266c064068835cb8c1e3fd3d215ec133
razvan666666 <razvan666666@gmail.com>
2025-02-05 02:50:33 +0200

    Update security.py

--- security.py
+++ security.py
@@ -7,5 +7,6 @@ def auth_barrier(password):
     hash_object = hashlib.sha256()
     hash_object.update(barrier.encode())
     hash_barrier = binascii.hexlify(hash_object.digest())
+    print (hash_barrier)
     return hash_barrier
     
